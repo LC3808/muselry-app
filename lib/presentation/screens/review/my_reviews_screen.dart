7b39e7799da2a3f62859c9cb154a20aa29e10c3c
LC3808 <1.0631485e+08+LC3808@users.noreply.github.com>
2026-08-06 11:21:30 +0000

    fix(v0.5.2): unused imports 제거 (my_reviews_screen)

--- lib/presentation/screens/review/my_reviews_screen.dart
+++ lib/presentation/screens/review/my_reviews_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../../domain/models/review.dart';
 import '../../../presentation/providers/review_provider.dart';
-import 'review_screen.dart' show ReviewFormSheet; // M7-G-6: 수정 시트 재사용 (ReviewFormSheet는 public)
 import 'review_edit_launcher.dart'; // v0.5.2: 공통 수정 launcher
 import '../../../config/router.dart';
 import '../../../domain/models/review_image.dart';
